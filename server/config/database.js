7eb71b5b46082c865b3a39e70a2a00bd94b8e7ac
Sammy Kordi <sammy.kordi@gmail.com>
2024-10-15 15:46:50 -0700

    Added Postgre Integration

--- server/config/database.js
+++ server/config/database.js
@@ -1,13 +1,27 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+// import { Sequelize } from 'sequelize';
+// import dotenv from 'dotenv';
 
-dotenv.config();  
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: console.log,  // This will log the SQL queries being run
-  });
+// dotenv.config();  
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     logging: console.log,  // This will log the SQL queries being run
+//   });
   
 
+// export default sequelize;
+
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false, // Render requires SSL in production
+    }
+  }
+});
+
 export default sequelize;
+
 
