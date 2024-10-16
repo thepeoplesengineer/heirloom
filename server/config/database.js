3a7ec982fbe211d4e5ad1aaf25942f946eab76ba
Sammy Kordi <sammy.kordi@gmail.com>
2024-10-15 18:32:02 -0700

    Added VITE API fixes

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 // import { Sequelize } from 'sequelize';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-// dotenv.config();  
+dotenv.config();  
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 //     host: process.env.DB_HOST,
 //     dialect: 'postgres',
