761778045799044651a2735fdf04b43a0148bae6
Sammy Kordi <sammy.kordi@gmail.com>
2024-10-17 09:21:47 -0700

    Fixed Packages and re-installed to build on Render

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_API_BASE_URL: string;
+    // add other env variables as needed
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
